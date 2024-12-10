b4a8eea4591368a1662db9e98b2cc811510f108a
Pratik Boharapi <pratik.boharapi@veritas.com>
2024-12-10 18:00:59 +0530

    first commit

---
+++ 1.provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_version = "<= 1.8.5" #Forcing which version of Terraform needs to be used
+  required_providers {
+    aws = {
+      version = "<= 6.0.0" #Forcing which version of plugin needs to be used.
+      source  = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket = "terraformpratik"
+    key    = "Ansible.tfstate"
+    region = "us-east-1"
+  }
+}
